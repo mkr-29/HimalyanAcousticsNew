2f3ef9ef1d2251f3c5dc9972c677030563620c9a
Mayank Kumar <mayank.k9802@gmail.com>
2023-03-15 20:04:34 +0530

    Landing

--- hima/src/Pages/Homepage/Components/Landing/Landing.jsx
+++ hima/src/Pages/Homepage/Components/Landing/Landing.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import "./Landing.css"
 
 export default function Landing() {
   return (
-    <div>Landing</div>
+    <div>
+        {/* random image from unsplash */}
+        <img className='landing-image' src="https://source.unsplash.com/random/1920x1080" alt="random image from unsplash" />
+    </div>
   )
 }
