959afaf07a3433bf36c18be57cafe0174eef3d02
Mayank Kumar <mayank.k9802@gmail.com>
2023-03-15 19:49:37 +0530

    Revert "Merge branch 'main' of https://github.com/mkr-29/HimalyanAcousticsNew"
    This reverts commit a370c7b2125a0538ea23548948dc8dd6f95c1760, reversing
    changes made to 479c882deeb5db1b16221a4dd729608ad7998b55.

--- hima/src/Pages/Homepage/Homepage.jsx
+++ hima/src/Pages/Homepage/Homepage.jsx
@@ -1,16 +1,10 @@
-import React from "react";
-import Navbar from "./Components/Navbar";
-import Certifications from "./Components/Certifications";
-import Footer from "./Components/Footer";
-import Achievements from "./Components/Achievements";
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
 
 export default function Homepage() {
   return (
     <div>
-      <Navbar />
-      <Achievements />
-      <Certifications />
-      <Footer />
+        <Navbar/>
     </div>
-  );
+  )
 }
