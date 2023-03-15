8f7ef346fa716ad78b95c80aa7c903b8feee5c23
Mayank Kumar <mayank.k9802@gmail.com>
2023-03-15 19:51:51 +0530

    C3

--- hima/src/Pages/Homepage/Homepage.jsx
+++ hima/src/Pages/Homepage/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Components/Navbar";
+import Navbar from "./Components/Navbar/Navbar";
 import Certifications from "./Components/Certifications";
 import Footer from "./Components/Footer";
 import Achievements from "./Components/Achievements";
