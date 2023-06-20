c020f6ef02ced9e94edb9a3095f63ace8dbad9ba
Mayank Kumar <mayank.k9802@gmail.com>
2023-06-20 18:23:24 +0530

    Inspiring Design Updates

--- hima/src/Pages/InspiringDesign/InspiringDesign.jsx
+++ hima/src/Pages/InspiringDesign/InspiringDesign.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import Footer from "../Homepage/Components/Footer/Footer";
 import Navbar from "../Homepage/Components/Navbar/Navbar";
 import "./InspiringDesign.css";
+import Img1 from "./Assets/img1.png";
+import Img2 from "./Assets/img2.png";
+import Img3 from "./Assets/img3.png";
+import Img4 from "./Assets/img4.png";
+import Img5 from "./Assets/img5.png";
+import Img6 from "./Assets/img6.png";
+import Img7 from "./Assets/img7.png";
+import Img8 from "./Assets/img8.png";
+import Img9 from "./Assets/img9.png";
+import Img10 from "./Assets/img10.png";
+import Img11 from "./Assets/img11.png";
+import Img12 from "./Assets/img12.png";
 
 export default function InspiringDesign() {
   return (
@@ -43,51 +55,51 @@ export default function InspiringDesign() {
       <div className="designs">
         <div className="des-1">
           <h2>#1 NRC</h2>
-          <img src="https://picsum.photos/500/500" alt="" />
+          <img src={Img1} alt="" />
         </div>
         <div className="des-1">
           <h2>#seamless chroma wall</h2>
-          <img src="https://picsum.photos/500/500" alt="" />
+          <img src={Img2} alt="" />
         </div>
         <div className="des-1">
           <h2>#plan system</h2>
-          <img src="https://picsum.photos/500/500" alt="" />
+          <img src={Img3} alt="" />
         </div>
         <div className="des-1">
           <h2>#exposed system</h2>
-          <img src="https://picsum.photos/500/500" alt="" />
+          <img src={Img4} alt="" />
         </div>
         <div className="des-1">
           <h2>#creative</h2>
-          <img src="https://picsum.photos/500/500" alt="" />
+          <img src={Img5} alt="" />
         </div>
         <div className="des-1">
           <h2>#wood</h2>
-          <img src="https://picsum.photos/500/500" alt="" />
+          <img src={Img6} alt="" />
         </div>
         <div className="des-1">
           <h2>#float</h2>
-          <img src="https://picsum.photos/500/500" alt="" />
+          <img src={Img7} alt="" />
         </div>
         <div className="des-1">
           <h2>#clouds & shape</h2>
-          <img src="https://picsum.photos/500/500" alt="" />
+          <img src={Img8} alt="" />
         </div>
         <div className="des-1">
           <h2>#baffles & cells</h2>
-          <img src="https://picsum.photos/500/500" alt="" />
+          <img src={Img9} alt="" />
         </div>
         <div className="des-1">
           <h2>#fabric</h2>
-          <img src="https://picsum.photos/500/500" alt="" />
+          <img src={Img10} alt="" />
         </div>
         <div className="des-1">
           <h2>#sound barrier (Co2 absorbent)</h2>
-          <img src="https://picsum.photos/500/500" alt="" />
+          <img src={Img11} alt="" />
         </div>
         <div className="des-1 des-2">
           <h2>#silent (acoustic drywall partion & door)</h2>
-          <img src="https://picsum.photos/500/500" alt="" />
+          <img src={Img12} alt="" />
         </div>
       </div>
       <Footer />
