bbd9eb720a13916c330a98f189b5f5a11c81258a
Mayank Kumar <mayank.k9802@gmail.com>
2023-06-20 19:38:17 +0530

    Projects

--- hima/src/Pages/Projects/Projects.jsx
+++ hima/src/Pages/Projects/Projects.jsx
@@ -2,11 +2,49 @@ import React from "react";
 import Navbar from "../Homepage/Components/Navbar/Navbar";
 import "./Projects.css";
 import Carousel1 from "./Carousel/Carousel1";
-import Image1 from "./Carousel/Assets/img1.jpg";
+import GP1 from "./Assets/GovernmentProjects/img1.png";
+import GP2 from "./Assets/GovernmentProjects/img2.png";
+import GP3 from "./Assets/GovernmentProjects/img3.png";
+import ALH1 from "./Assets/AuditoriumLH/img1.png";
+import ALH2 from "./Assets/AuditoriumLH/img2.png";
+import ALH3 from "./Assets/AuditoriumLH/img3.png";
+import CMH1 from "./Assets/ConventionMH/img1.png";
+import CMH2 from "./Assets/ConventionMH/img2.png";
+import CMH3 from "./Assets/ConventionMH/img3.png";
+import CO1 from "./Assets/CorporateOffice/img1.png"
+import CO2 from "./Assets/CorporateOffice/img2.png"
+import CO3 from "./Assets/CorporateOffice/img3.png"
+import E1 from "./Assets/Education/img1.png";
+import E2 from "./Assets/Education/img2.png";
+import E3 from "./Assets/Education/img3.png";
+import CHC1 from "./Assets/CommercialHC/img1.png";
+import CHC2 from "./Assets/CommercialHC/img2.png";
+import CHC3 from "./Assets/CommercialHC/img3.png";
+import S1 from "./Assets/Studio/img1.png";
+import S2 from "./Assets/Studio/img2.png";
+import S3 from "./Assets/Studio/img3.png";
+import WP1 from "./Assets/WorshipPlaces/img1.png";
+import WP2 from "./Assets/WorshipPlaces/img2.png";
+import WP3 from "./Assets/WorshipPlaces/img3.png";
+import SC1 from "./Assets/SportsComplex/img1.png";
+import SC2 from "./Assets/SportsComplex/img2.png";
+import SC3 from "./Assets/SportsComplex/img3.png";
+import HC1 from "./Assets/Healthcare/img1.png";
+import HC2 from "./Assets/Healthcare/img2.png";
+import HC3 from "./Assets/Healthcare/img3.png";
 import Footer from "../Homepage/Components/Footer/Footer";
 
 export default function Projects() {
-    const images1=[Image1,Image1,Image1,Image1]
+    const govtP=[GP1,GP2,GP3,GP1];
+    const audLH=[ALH1,ALH2,ALH3,ALH1];
+    const conMH=[CMH1,CMH2,CMH3,CMH1];
+    const corpO=[CO1,CO2,CO3,CO1];
+    const educa=[E1,E2,E3,E1];
+    const comHC=[CHC1,CHC2,CHC3,CHC1];
+    const studi=[S1,S2,S3,S1];
+    const worhP=[WP1,WP2,WP3,WP1];
+    const sporC=[SC1,SC2,SC3,SC1];
+    const healC=[HC1,HC2,HC3,HC1];
   return (
     <div>
       <Navbar />
@@ -47,61 +85,61 @@ export default function Projects() {
         <div className="proj-1">
           <h2>#government projects</h2>
           <Carousel1
-            images={images1}
+            images={govtP}
           />
         </div>
         <div className="proj-1">
           <h2>#auditorium & lecture hall</h2>
           <Carousel1
-            images={images1}
+            images={audLH}
           />
         </div>
         <div className="proj-1">
           <h2>#convention & multipurpose hall</h2>
           <Carousel1
-            images={images1}
+            images={conMH}
           />
         </div>
         <div className="proj-1">
           <h2>#corporate office</h2>
           <Carousel1
-            images={images1}
+            images={corpO}
           />
         </div>
         <div className="proj-1">
           <h2>#education</h2>
           <Carousel1
-            images={images1}
+            images={educa}
           />
         </div>
         <div className="proj-1">
           <h2>#commercial & home cinemas</h2>
           <Carousel1
-            images={images1}
+            images={comHC}
           />
         </div>
         <div className="proj-1">
           <h2>#studio</h2>
           <Carousel1
-            images={images1}
+            images={studi}
           />
         </div>
         <div className="proj-1">
           <h2>#worship place</h2>
           <Carousel1
-            images={images1}
+            images={worhP}
           />
         </div>
         <div className="proj-1">
           <h2>#sports complex (stadium, gym, swimming area and shooting range)</h2>
           <Carousel1
-            images={images1}
+            images={sporC}
           />
         </div>
         <div className="proj-1">
           <h2>#healthcare</h2>
           <Carousel1
-            images={images1}
+            images={healC}
           />
         </div>
       </div>
