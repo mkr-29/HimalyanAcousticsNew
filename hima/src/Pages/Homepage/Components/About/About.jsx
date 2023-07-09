aceead5b784aafbb7ed124ab43835195adfe0f33
Mayank Kumar <mayank.k9802@gmail.com>
2023-07-09 18:58:57 +0530

    About Fixed

--- hima/src/Pages/Homepage/Components/About/About.jsx
+++ hima/src/Pages/Homepage/Components/About/About.jsx
@@ -2,32 +2,35 @@ import React from "react";
 import "./About.css";
 import { Link } from "react-router-dom";
 import Asset1 from "./Assets/asset1.png";
+import Asset2 from "./Assets/asset2.png";
 
 export default function About() {
   return (
     <div className="about">
-      <div className="about-1 flex px-24 gap-20">
+      <div className="about-1 flex flex-col sm:flex-row px-4 sm:px-24 sm:gap-20">
         <div className="div-1 flex flex-col w-1/2">
           <h1 className="about-head">
             acoustic
             <br />
             meet's design!!
           </h1>
-          <span className="about-desc">
-            In years past, acoustic treatments were all about form and function.
-            But new materials are changing the way the wall and ceiling industry
-            can use acoustic treatments as part of the aesthetics of a space,
-            and the sky is the limit.
-            <br />
-            <br />
-            Acoustical treatment solutions are no exception, and play an
-            important role in the redesign of workspaces, schools, healthcare
-            facilities, hospitality venues, and so on.
-            <br />
-            <br />
-            ents are just as important as décor and aesthetics & décor while
-            controlling sound.
-            <Link to="/"> Read More...</Link>
+          <span className="about-desc text-xs xl:text-sm 2xl:text-[17px] flex flex-col gap-5">
+            <span className="font-normal">
+              In years past, acoustic treatments were all about form and
+              function. But new materials are changing the way the wall and
+              ceiling industry can use acoustic treatments as part of the
+              aesthetics of a space, and the sky is the limit.
+            </span>
+            <span>
+              Acoustical treatment solutions are no exception, and play an
+              important role in the redesign of workspaces, schools, healthcare
+              facilities, hospitality venues, and so on.
+            </span>
+            <span>
+              Vents are just as important as décor and aesthetics & décor while
+              controlling sound.
+              <Link to="/"> Read More...</Link>
+            </span>
           </span>
         </div>
         <div className="div-1 flex flex-col w-1/2">
@@ -36,7 +39,7 @@ export default function About() {
             <br />
             based solutions
           </h1>
-          <div className="flex gap-10">
+          <div className="flex 2xl:gap-10 gap-2">
             <div className="banner flex flex-col justify-centre 2xl:w-1/5 justify-center items-center w-36">
               <img src={Asset1} alt="" className="max-w-[6.8rem]" />
               <div className="banner-in flex flex-col items-center text-white">
@@ -58,8 +61,8 @@ export default function About() {
                 <span className="leading-5 text-black">acoustic</span>
                 <span className="text-black leading-4">performance</span>
                 <span className="text-black text-xl leading-4">with our</span>
-                <span className="text-4xl leading-8">1 NRC</span>
-                <span className="leading-3">LAB-CERTIFIED</span>
+                <span className="text-[2rem] leading-8">1 NRC</span>
+                <span className="leading-3 text-[12.2px]">LAB-CERTIFIED</span>
                 <span className="leading-10">panel</span>
               </div>
             </div>
@@ -96,8 +99,29 @@ export default function About() {
           </div>
         </div>
       </div>
-      <div className="about-left">
-        <div className="div-2">
+      <div className="about-2 sm:px-24 flex gap-20">
+        <div className="flex w-1/2 items-center">
+          <img src={Asset2} alt="" className="w-[40%]" />
+          <span className="flex flex-col text-[#818181] font-[Arial] text-xl">
+            <span>
+              <span className="font-semibold">HIMALYAN ACOUSTICS</span> is proud
+            </span>
+            <span>
+              to launch <span className="font-semibold">CARBON ADSORBER</span>
+            </span>
+            <span>
+              <span className="font-semibold">PANELS</span>, first of its kinds panels
+            </span>
+            <span>
+              high on performance for
+            </span>
+            <span>
+              NOISE + AIR POLLUTION
+            </span>
+            <Link className="text-lg font-semibold text-[#E23424]" to="">Read More...</Link>
+          </span>
+        </div>
+        <div className="w-1/2 flex flex-col gap-2">
           <h1 className="about-sub-head mt-10">
             INDOOR AIR QUALITY
             <br />
@@ -112,14 +136,6 @@ export default function About() {
             <Link to="/">Read More...</Link>
           </span>
         </div>
-      </div>
-      <div className="about-left about-right">
-        <div className="div-1"></div>
-      </div>
-      <div className="div-2 mt-8">
-        <h3 className="2xl:p-3 p-1 text-center text-white text-2xl 2xl:text-3xl mt-6">
-          featured projects
-        </h3>
       </div>
     </div>
   );
