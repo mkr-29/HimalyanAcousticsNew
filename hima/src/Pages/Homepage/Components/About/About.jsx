3c72cac6c9760b9e4c50af0772abee38478af907
Mayank Kumar <mayank.k9802@gmail.com>
2023-07-10 13:34:09 +0530

    About Fixes

--- hima/src/Pages/Homepage/Components/About/About.jsx
+++ hima/src/Pages/Homepage/Components/About/About.jsx
@@ -7,13 +7,13 @@ import Asset2 from "./Assets/asset2.png";
 export default function About() {
   return (
     <div className="about">
-      <div className="about-1 gap-7 flex flex-col lg:flex-row px-4 md:px-24 md:gap-20 md:flex-col">
+      <div className="about-1 gap-7 flex flex-col lg:flex-row px-4 sm:px-8 lg:px-14 lg:gap-20">
         <div className="div-1 flex flex-col 2xl:w-1/2 w-full px-2">
-          <h1 className="about-head text-[30px] leading-7 flex flex-col sm:flex-row sm:gap-2 lg:flex-col md:text-[40px] md:leading-8 md:m-0">
+          <h1 className="about-head text-[30px] leading-7 flex flex-col sm:flex-row sm:gap-2 lg:flex-col md:text-[30px] md:leading-8 md:m-0 lg:leading-6">
             acoustic
             <span> meet's design!!</span>
           </h1>
-          <span className="about-desc text-xs flex flex-col gap-2 leading-5 text-justify sm:text-[14px] sm:leading-6 xl:text-[17px] 2xl:text-[17px]">
+          <span className="about-desc text-xs flex flex-col gap-2 leading-5 text-justify sm:text-[14px] sm:leading-6 lg:text-[12px] xl:text-[16px]">
             <span className="font-normal">
               In years past, acoustic treatments were all about form and
               function. But new materials are changing the way the wall and
@@ -33,7 +33,7 @@ export default function About() {
           </span>
         </div>
         <div className="div-1 flex flex-col 2xl:w-1/2 w-full px-2">
-          <h1 className="about-head mb-10 text-[30px] leading-7 flex flex-col gap-1 sm:flex-row sm:gap-2">
+          <h1 className="about-head mb-10 text-[30px] leading-7 flex flex-col gap-1 sm:flex-row sm:gap-2 lg:flex-col">
             performance
             <span>based solutions</span>
           </h1>
@@ -95,21 +95,21 @@ export default function About() {
                 className="max-w-[5rem] sm:max-w-[6rem]"
               />
               <div className="banner-in2 flex flex-col items-center text-white top-2">
-                <span className="text-[8px] leading-4 text-black">
+                <span className="text-[8px] leading-4 text-black sm:text-[10px]">
                   get the best of
                 </span>
-                <span className="text-[13.5px] leading-3">ACOUSTIC</span>
-                <span className="text-[10.1px] leading-3">PERFORMANCE</span>
-                <span className="text-black text-[19.5px] leading-4">
+                <span className="text-[13.5px] leading-3 sm:text-[16px] sm:leading-4">ACOUSTIC</span>
+                <span className="text-[10.1px] leading-3 sm:text-[12px] sm:leading-4">PERFORMANCE</span>
+                <span className="text-black text-[19.5px] leading-4 sm:text-[23px]">
                   with our
                 </span>
-                <span className="text-[20px] leading-4">SILENT</span>
-                <span className="leading-3 text-[7.5px]">ACOUSTIC DRYWALL</span>
-                <span className="leading-4 text-[17.5px]">Partition</span>
-                <span className="leading-3 text-black text-[15.3px]">
+                <span className="text-[20px] leading-4 sm:text-[23.5px] sm:leading-7">SILENT</span>
+                <span className="leading-3 text-[7.5px] sm:text-[8.8px]">ACOUSTIC DRYWALL</span>
+                <span className="leading-4 text-[17.5px] sm:text-[20.6px] sm:leading-5">Partition</span>
+                <span className="leading-3 text-black text-[15.3px] sm:text-[18px]">
                   range with
                 </span>
-                <span className="leading-3 text-[12px]">UPTO 65 STC</span>
+                <span className="leading-3 text-[12px] sm:text-[14.35px] sm:leading-6">UPTO 65 STC</span>
               </div>
             </div>
             <div className="banner flex flex-col justify-centre 2xl:w-auto justify-center items-center w-36">
@@ -119,32 +119,32 @@ export default function About() {
                 className="max-w-[5rem] sm:max-w-[6rem]"
               />
               <div className="banner-in3 flex flex-col items-center text-white top-2">
-                <span className="text-[14px] leading-3 text-black">
+                <span className="text-[14px] leading-3 text-black sm:text-[16.2px]">
                   featuring
                 </span>
-                <span className="leading-4 text-[11px] text-black">
+                <span className="leading-4 text-[11px] text-black sm:text-[13px] sm:leading-5">
                   remarkably
                 </span>
-                <span className="leading-3 text-[9px]">SUSTAINABLE &</span>
-                <span className="leading-3 text-[7.8px]">GREEN CERTIFIED</span>
-                <span className="leading-3 text-[8.9px]">CEMENT FIBRES</span>
-                <span className="leading-4 text-black text-[16px]">
+                <span className="leading-3 text-[9px] sm:text-[11px] sm:leading-4">SUSTAINABLE &</span>
+                <span className="leading-3 text-[7.8px] sm:text-[9.5px] sm:leading-4">GREEN CERTIFIED</span>
+                <span className="leading-3 text-[8.9px] sm:text-[10.8px] sm:leading-4">CEMENT FIBRES</span>
+                <span className="leading-4 text-black text-[16px] sm:text-[18.6px]">
                   one of the
                 </span>
-                <span className="leading-4 text-black text-[10.7px]">
+                <span className="leading-4 text-black text-[10.7px] sm:text-[12.5px] sm:leading-5">
                   1ST IN INDIA
                 </span>
-                <span className="leading-4 text-[17px]">APHONY</span>
-                <span className="leading-4 text-[14.8px]">FIBRETTE</span>
+                <span className="leading-4 text-[17px] sm:text-[19.4px] sm:leading-5">APHONY</span>
+                <span className="leading-4 text-[14.8px] sm:text-[17px] sm:leading-5">FIBRETTE</span>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="about-2 flex flex-col px-2 mt-2 gap-10 sm:px-24 sm:gap-20">
-        <div className="flex w-full items-center sm:w-1/2">
-          <img src={Asset2} alt="" className="w-[38%]" />
-          <span className="flex flex-col text-[#818181] text-[14px] font-[Arial] 2xl:text-xl">
+      <div className="about-2 flex flex-col px-2 mt-2 gap-10 sm:px-8 lg:px-14 lg:gap-20 lg:flex-row">
+        <div className="flex w-full items-center lg:w-1/2">
+          <img src={Asset2} alt="" className="w-[38%] lg:w-[36%]" />
+          <span className="flex flex-col text-[#818181] text-[14px] font-[Arial] sm:text-[22px] md:text-[24px] lg:text-[18px] xl:text-[22px]">
             <span>
               <span className="font-semibold">HIMALYAN ACOUSTICS</span> is proud
             </span>
@@ -157,26 +157,26 @@ export default function About() {
             </span>
             <span>high on performance for</span>
             <span>NOISE + AIR POLLUTION</span>
-            <Link className="text-[14px] font-semibold text-[#E23424]" to="">
+            <Link className="text-[14px] font-semibold text-[#E23424] sm:text-[16px]" to="">
               Read More...
             </Link>
           </span>
         </div>
-        <div className="flex flex-col gap-2 justify-center sm:w-1/2">
-          <h1 className="about-sub-head text-[24px] leading-5">
+        <div className="flex flex-col gap-2 justify-center lg:w-1/2">
+          <h1 className="about-sub-head text-[24px] leading-5 sm:text-[28px] sm:leading-7">
             INDOOR AIR QUALITY
             <br />
-            <span className="text-[22px] leading-4">
+            <span className="text-[22px] leading-4 sm:text-[24px] sm:leading-5 md:text-[24px] lg:text-[26px]">
               at our best green standards
             </span>
           </h1>
-          <span className="about-desc text-justify max-[1250px]:text-[14px]">
+          <span className="about-desc text-justify max-[1250px]:text-[14px] xl:text-[16px]">
             Environmental noise and air pollution, as well as poor green
             infrastructure quality, are major concerns for the new spaces being
             designed in the contemporary era.This issue is further stressed for
             urban areas located close to aggressive sources of such pollutants,
             such as airports, railways, highways, or leisure areas.
-            <Link to="/" className="max-[1250px]:text-[12px]">
+            <Link to="/" className="">
               Read More...
             </Link>
           </span>
