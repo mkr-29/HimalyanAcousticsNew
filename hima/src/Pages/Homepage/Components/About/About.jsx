ce28d9f5e0205b60e0e66242ef61ca730bef2baa
Mayank Kumar <mayank.k9802@gmail.com>
2023-07-10 12:50:23 +0530

    About Fixes

--- hima/src/Pages/Homepage/Components/About/About.jsx
+++ hima/src/Pages/Homepage/Components/About/About.jsx
@@ -7,13 +7,13 @@ import Asset2 from "./Assets/asset2.png";
 export default function About() {
   return (
     <div className="about">
-      <div className="about-1 gap-7 flex flex-col lg:flex-row px-4 sm:px-24 sm:gap-20 md:flex-col">
+      <div className="about-1 gap-7 flex flex-col lg:flex-row px-4 md:px-24 md:gap-20 md:flex-col">
         <div className="div-1 flex flex-col 2xl:w-1/2 w-full px-2">
-          <h1 className="about-head text-[30px] leading-7 flex md:gap-3 flex-col lg:flex-col md:text-[40px] md:leading-8 md:m-0">
-            acoustic 
+          <h1 className="about-head text-[30px] leading-7 flex flex-col sm:flex-row sm:gap-2 lg:flex-col md:text-[40px] md:leading-8 md:m-0">
+            acoustic
             <span> meet's design!!</span>
           </h1>
-          <span className="about-desc text-xs xl:text-[17px] md:text-[18px] leading-6 max-[1250px]:leading-[1.2rem] text-justify 2xl:text-[17px] flex flex-col gap-2">
+          <span className="about-desc text-xs flex flex-col gap-2 leading-5 text-justify sm:text-[14px] sm:leading-6 xl:text-[17px] 2xl:text-[17px]">
             <span className="font-normal">
               In years past, acoustic treatments were all about form and
               function. But new materials are changing the way the wall and
@@ -33,38 +33,33 @@ export default function About() {
           </span>
         </div>
         <div className="div-1 flex flex-col 2xl:w-1/2 w-full px-2">
-          <h1 className="about-head mb-10 text-[30px] leading-7">
+          <h1 className="about-head mb-10 text-[30px] leading-7 flex flex-col gap-1 sm:flex-row sm:gap-2">
             performance
-            <br />
-            based solutions
+            <span>based solutions</span>
           </h1>
           <div className="flex justify-between gap-1">
             <div className="banner flex flex-col justify-centre 2xl:w-auto justify-center items-center w-36">
               <img
                 src={Asset1}
                 alt=""
-                className="max-w-[5rem] sm:max-w-[5rem] min-[724px]:max-w-[5.5rem]"
+                className="max-w-[5rem] sm:max-w-[6rem]"
               />
-              <div className="banner-in max-[1250px]:top-2 flex flex-col items-center text-white">
-                <span className="text-[14.5px] leading-3">
-                  ONE OF
-                </span>
-                <span className="text-[13px] leading-4">
-                  INDIA'S
-                </span>
+              <div className="banner-in top-2 flex flex-col items-center text-white">
+                <span className="text-[14.5px] leading-3 sm:text-[17px]">ONE OF</span>
+                <span className="text-[13px] leading-4 sm:text-[15px] sm:leading-6">INDIA'S</span>
                 <span className="text-black text-[32px] leading-7">
                   CO<sub className="text-sm">2</sub>
                 </span>
-                <span className="text-black text-[12px] leading-3">
+                <span className="text-black text-[12px] leading-3 sm:leading-5">
                   absorbant
                 </span>
-                <span className="text-2xl leading-5 max-[1250px]:text-[20px]">
+                <span className="text-[19.7px] leading-5 sm:text-[23px]">
                   SOUND
                 </span>
-                <span className="leading-4 max-[1250px]:text-[16px]">
+                <span className="leading-4 sm:text-[18.5px] sm:leading-6">
                   BARRIER
                 </span>
-                <span className="leading-5 max-[1250px]:text-[22px]">
+                <span className="leading-5 text-[22px] sm:text-[26px]">
                   system
                 </span>
               </div>
@@ -73,100 +68,80 @@ export default function About() {
               <img
                 src={Asset1}
                 alt=""
-                className="max-w-[5rem] sm:max-w-[5rem] min-[724px]:max-w-[5.5rem]"
+                className="max-w-[5rem] sm:max-w-[6rem]"
               />
-              <div className="banner-in1 flex flex-col items-center text-white max-[1250px]:top-2">
-                <span className="text-[15px] leading-3 text-black">excellent</span>
-                <span className="text-[16px] leading-4 text-black">acoustic</span>
-                <span className="text-[10.6px] text-black leading-3">
+              <div className="banner-in1 flex flex-col items-center text-white top-2">
+                <span className="text-[15px] leading-3 text-black sm:text-[17px] sm:leading-4">
+                  excellent
+                </span>
+                <span className="text-[16px] leading-4 text-black sm:text-[18px] sm:leading-6">
+                  acoustic
+                </span>
+                <span className="text-[10.6px] text-black leading-3 sm:text-[11.6px] sm:leading-4">
                   performance
                 </span>
-                <span className="text-black text-[18px] leading-5">with our</span>
-                <span className="text-[22px] leading-6">
-                  1 NRC
-                </span>
-                <span className="leading-3 text-[8.2px]">
-                  LAB-CERTIFIED
-                </span>
-                <span className="text-[28px] leading-7">
-                  panel
-                </span>
-              </div>
-            </div>
-            <div className="banner flex flex-col justify-centre 2xl:w-auto justify-center items-center w-36">
-              <img
-                src={Asset1}
-                alt=""
-                className="max-w-[5rem] sm:max-w-[5rem] min-[724px]:max-w-[5.5rem]"
-              />
-              <div className="banner-in2 flex flex-col items-center text-white max-[1250px]:top-2">
-                <span className="text-[10px] leading-4 text-black">
-                  get the best of
-                </span>
-                <span className="text-[12.3px] leading-3">
-                  ACOUSTIC
-                </span>
-                <span className="text-[9.3px] leading-3">
-                  PERFORMANCE
-                </span>
-                <span className="text-black text-[18px] leading-4">
+                <span className="text-black text-[16.5px] leading-5 sm:text-[17.8px] sm:leading-6">
                   with our
                 </span>
-                <span className="text-[20px] leading-4">
-                  SILENT
-                </span>
-                <span className="leading-3 text-[7.5px]">
-                  ACOUSTIC DRYWALL
-                </span>
-                <span className="leading-4 text-[17.5px]">
-                  Partition
-                </span>
-                <span className="leading-3 text-black text-[12px]">
-                  range with
-                </span>
-                <span className="leading-3 text-[9.5px]">
-                  UPTO 65 STC
-                </span>
+                <span className="text-[24px] leading-6 sm:text-[28px] sm:leading-7">1 NRC</span>
+                <span className="leading-3 text-[9.4px] sm:text-[10.7px] sm:leading-4">LAB-CERTIFIED</span>
+                <span className="text-[31px] leading-7 sm:text-[36px]">panel</span>
               </div>
             </div>
             <div className="banner flex flex-col justify-centre 2xl:w-auto justify-center items-center w-36">
               <img
                 src={Asset1}
                 alt=""
-                className="max-w-[5rem] sm:max-w-[5rem] min-[724px]:max-w-[5.5rem]"
+                className="max-w-[5rem] sm:max-w-[6rem]"
               />
-              <div className="banner-in3 flex flex-col items-center text-white max-[1250px]:top-2">
-                <span className="text-[14px] leading-3 text-black">featuring</span>
+              <div className="banner-in2 flex flex-col items-center text-white top-2">
+                <span className="text-[8px] leading-4 text-black">
+                  get the best of
+                </span>
+                <span className="text-[13.5px] leading-3">ACOUSTIC</span>
+                <span className="text-[10.1px] leading-3">PERFORMANCE</span>
+                <span className="text-black text-[19.5px] leading-4">
+                  with our
+                </span>
+                <span className="text-[20px] leading-4">SILENT</span>
+                <span className="leading-3 text-[7.5px]">ACOUSTIC DRYWALL</span>
+                <span className="leading-4 text-[17.5px]">Partition</span>
+                <span className="leading-3 text-black text-[15.3px]">
+                  range with
+                </span>
+                <span className="leading-3 text-[12px]">UPTO 65 STC</span>
+              </div>
+            </div>
+            <div className="banner flex flex-col justify-centre 2xl:w-auto justify-center items-center w-36">
+              <img
+                src={Asset1}
+                alt=""
+                className="max-w-[5rem] sm:max-w-[6rem]"
+              />
+              <div className="banner-in3 flex flex-col items-center text-white top-2">
+                <span className="text-[14px] leading-3 text-black">
+                  featuring
+                </span>
                 <span className="leading-4 text-[11px] text-black">
                   remarkably
                 </span>
-                <span className="leading-3 text-[9px]">
-                  SUSTAINABLE &
-                </span>
-                <span className="leading-3 text-[7.8px]">
-                  GREEN CERTIFIED
-                </span>
-                <span className="leading-3 text-[8.9px]">
-                  CEMENT FIBRES
-                </span>
+                <span className="leading-3 text-[9px]">SUSTAINABLE &</span>
+                <span className="leading-3 text-[7.8px]">GREEN CERTIFIED</span>
+                <span className="leading-3 text-[8.9px]">CEMENT FIBRES</span>
                 <span className="leading-4 text-black text-[16px]">
                   one of the
                 </span>
                 <span className="leading-4 text-black text-[10.7px]">
                   1ST IN INDIA
                 </span>
-                <span className="leading-4 text-[17px]">
-                  APHONY
-                </span>
-                <span className="leading-4 text-[14.8px]">
-                  FIBRETTE
-                </span>
+                <span className="leading-4 text-[17px]">APHONY</span>
+                <span className="leading-4 text-[14.8px]">FIBRETTE</span>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="about-2 flex flex-col px-2 mt-2 gap-8 sm:px-24 sm:gap-20">
+      <div className="about-2 flex flex-col px-2 mt-2 gap-10 sm:px-24 sm:gap-20">
         <div className="flex w-full items-center sm:w-1/2">
           <img src={Asset2} alt="" className="w-[38%]" />
           <span className="flex flex-col text-[#818181] text-[14px] font-[Arial] 2xl:text-xl">
@@ -188,10 +163,10 @@ export default function About() {
           </span>
         </div>
         <div className="flex flex-col gap-2 justify-center sm:w-1/2">
-          <h1 className="about-sub-head max-[1250px]:text-[34px] max-[1250px]:leading-7">
+          <h1 className="about-sub-head text-[24px] leading-5">
             INDOOR AIR QUALITY
             <br />
-            <span className="max-[1250px]:text-[28px] max-[1250px]:leading-4">
+            <span className="text-[22px] leading-4">
               at our best green standards
             </span>
           </h1>
