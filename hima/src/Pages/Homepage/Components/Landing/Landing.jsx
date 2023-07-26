5bfda574f32ac741d028ae52287d52d5f5b48659
Mayank Kumar <mayank.k9802@gmail.com>
2023-07-26 19:41:42 +0530

    Update Landing.jsx

--- hima/src/Pages/Homepage/Components/Landing/Landing.jsx
+++ hima/src/Pages/Homepage/Components/Landing/Landing.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import "./Landing.css"
+import React from "react";
+import "./Landing.css";
+import BG from "./Assets/landing.png";
 
 export default function Landing() {
   return (
     <div>
-        <div className='landing-image'/>
+      <img src={BG} className="px-16" alt="" />
     </div>
-  )
+  );
 }
