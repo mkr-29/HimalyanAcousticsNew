6bb9112d6b9f9edf36429474e6ee27941d8594f8
Mayank Kumar <mayank.k9802@gmail.com>
2023-07-28 20:16:47 +0530

    Update Landing.jsx

--- hima/src/Pages/Homepage/Components/Landing/Landing.jsx
+++ hima/src/Pages/Homepage/Components/Landing/Landing.jsx
@@ -4,7 +4,7 @@ import BG from "./Assets/landing.png";
 
 export default function Landing() {
   return (
-    <div className="bg-[#ebebeb] mt-[6.6rem]">
+    <div className=" mt-[6.6rem]">
       <img src={BG} className="px-16 w-full" alt="" />
     </div>
   );
